eb7e8010dea6931ddd2e299686988e4968b1fbbd
itsbillw <itsbillw@gmail.com>
2018-05-16 00:24:39 -0400

    Update for compare app. results to json. need html formatting

--- csv_compare/views.py
+++ csv_compare/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import pandas as pd
+import numpy as np
 
 def index(request):
     """The home page for learning log"""
@@ -14,4 +16,28 @@ def compare(request):
 
 def result(request):
     """import and compare two csv files """
-    return render(request, 'csv_compare/result.html')
+    if request.POST and request.FILES:
+        csvfile1 = request.FILES['csv_file1']
+        ds1 = pd.read_csv(csvfile1)
+        csvfile2 = request.FILES['csv_file2']
+        ds2 = pd.read_csv(csvfile2)
+
+        def compare_two_dfs(input_df_1, input_df_2):
+            df_1, df_2 = input_df_1.copy(), input_df_2.copy()
+            ne_stacked = (df_1 != df_2).stack()
+            changed = ne_stacked[ne_stacked]
+            changed.index.names = ['id', 'col']
+            difference_locations = np.where(df_1 != df_2)
+            changed_from = df_1.values[difference_locations]
+            changed_to = df_2.values[difference_locations]
+            df = pd.DataFrame({
+                'from': changed_from,
+                'to': changed_to
+            }, index=changed.index)
+            return df
+
+        ds3 = compare_two_dfs(ds1, ds2)
+        ds3 = ds3.to_json
+        context = {'ds3': ds3}
+
+    return render(request, 'csv_compare/result.html', context)
